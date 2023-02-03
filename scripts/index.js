749449d4e4eb224ef605f34d02772b96f35b1552
bababum <bababum1995@gmail.com>
2023-02-03 13:14:34 +0700

    sub for burger

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
     $('.body').toggleClass('lock');
     });
 });
+
+
+let buttonsSubMenu = document.querySelectorAll('.header__btn-sub-menu');
+if (buttonsSubMenu.length > 0) {
+    for (let index = 0; index < buttonsSubMenu.length; index++) {
+        const buttonSubMenu = buttonsSubMenu[index];
+        buttonSubMenu.addEventListener('click', function(e) {
+            buttonSubMenu.parentElement.classList.toggle('active')
+            buttonSubMenu.classList.toggle('active')
+        });
+    }
+}
