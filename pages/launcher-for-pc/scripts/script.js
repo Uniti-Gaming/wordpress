a4b13fb345b934835f364e55314fb9251476f844
bababum <bababum1995@gmail.com>
2023-02-19 09:45:50 +0700

    adaptiv slider for mobile

--- pages/launcher-for-pc/scripts/script.js
+++ pages/launcher-for-pc/scripts/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
     $('.slider').slick({
         slidesToShow:4,
-        infinite:false
+        infinite:false,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1.9
+                }
+              },
+              {
+              breakpoint: 400,
+              settings: {
+                slidesToShow: 1.6
+              }
+            },
+            {
+              breakpoint: 355,
+              settings: {
+                slidesToShow: 1.4
+              }
+            },
+          ]
     });
 });
