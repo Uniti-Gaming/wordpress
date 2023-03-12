690f4eb84259a293cecf2e6b59cfdd9a3ce404a9
bababum <bababum1995@gmail.com>
2023-03-12 23:59:18 +0700

    adaptive for ipad plus animation

--- pages/launcher/pages/launcher-for-pc/scripts/script.js
+++ pages/launcher/pages/launcher-for-pc/scripts/script.js
@@ -1,26 +1,62 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow:4,
-        infinite:false,
-        responsive: [
-            {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1.9
-                }
-              },
-              {
-              breakpoint: 400,
-              settings: {
-                slidesToShow: 1.6
-              }
-            },
-            {
-              breakpoint: 355,
-              settings: {
-                slidesToShow: 1.4
-              }
-            },
-          ]
-    });
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3.6
+        }
+      },
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3.2
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2.7
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2.7
+        }
+      },
+      {
+        breakpoint: 660,
+        settings: {
+          slidesToShow: 2.3
+        }
+      },
+      {
+        breakpoint: 570,
+        settings: {
+          slidesToShow: 1.9
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1.6
+        }
+      },
+      {
+        breakpoint: 355,
+        settings: {
+          slidesToShow: 1.4
+        }
+      },
+    ]
+  });
 });
